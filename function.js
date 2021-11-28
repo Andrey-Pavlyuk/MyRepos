cab5c1628db9d254e08c55be56f09e196bf94420
Andrey-Pavlyuk <72155991+Andrey-Pavlyuk@users.noreply.github.com>
2021-11-28 17:05:59 +0300

    Lesson3 version 1.2

--- function.js
+++ function.js
@@ -1,10 +1,10 @@
 function myFunc() {
     while (true) {
-        prompt('Имя:')
-        prompt('Возраст:')
-        prompt('Ваша професия:')
-        prompt('Любимое блюдо:')
-        if(window.confirm('Потвердить данные')){
+        let name = prompt('Имя:')
+        let age = prompt('Возраст:')
+        let profession = prompt('Ваша професия:')
+        let dish = prompt('Любимое блюдо:')
+        if(window.confirm(`Потвердить данные:\nИмя: ${name}\nВозраст: ${age}\nПрофессия: ${profession}\nЛюбимое блюдо: ${dish}`)){
             break
         }
     }
